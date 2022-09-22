c0904d8ebd130e45b38029ca18aa0dbe3c250f39
Kush0301 <kushagra2000@gmail.com>
2022-09-22 23:16:31 +0530

    Removed plan shape

--- planShape/LEDNet/train/train_lednet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-dataset_dir=/home/kushagra/IIIT-H/PipesDataset/PipesDatasetUnshuffled
-
-python main.py --savedir logs --datadir $dataset_dir --num-epochs 2 --batch-size 2 --num-workers 2 --epochs-save 1 --resume 
