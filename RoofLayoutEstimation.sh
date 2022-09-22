9c73f469484fc557adb6caa06d95a2e336b02f55
Kush0301 <kushagra2000@gmail.com>
2022-09-22 14:19:16 +0530

    Model weights will only be downloaded once and not all the time

--- RoofLayoutEstimation.sh
+++ RoofLayoutEstimation.sh
@@ -13,7 +13,16 @@ echo "Done!"
 touch $results_path/out.log
 echo "Estimating the roof masks"
 cd LEDNet/save/logs
-gdown 1E-FLi3byKxCcfKAGfqWAkp7Li4poKdKO
+
+# Check if the model weights have been downloaded before
+if test -f "model_best.pth"; then
+    echo "Model weights exist"
+else
+    echo "Model weights do not exist"
+    echo "Downloading model weights"
+    gdown 1E-FLi3byKxCcfKAGfqWAkp7Li4poKdKO
+    echo "Done!"
+fi
 cd ../../..
 cd LEDNet/test
 chmod 777 test.py
@@ -31,7 +40,14 @@ echo "Estimating the NSE Masks"
 rm -rf ObjectMasks
 mkdir ObjectMasks
 cd Detic
-gdown 1RS2a1V-Gm2c4j7PbR9qg_xdzI1ohj6lz
+if test -f "Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth"; then
+    echo "Model weights exist"
+else
+    echo "Model weights do not exist"
+    echo "Downloading model weights"
+    gdown 1RS2a1V-Gm2c4j7PbR9qg_xdzI1ohj6lz
+    echo "Done!"
+fi
 chmod 777 demo.py
 python demo.py --config-file configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml --input ../images/*.png --output ../ObjectMasks --vocabulary custom --custom_vocabulary solar_array,air_conditioner,vent,box,sink --confidence-threshold 0.5 --opts MODEL.WEIGHTS Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth >> ../$results_path/out.log
 cd ..
